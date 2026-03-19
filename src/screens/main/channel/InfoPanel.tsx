56f332cf932d4fe69883c0d45aebbf8b500be3fb
laconite <laconite2006@gmail.com>
2026-03-19 14:53:45 +0400

    The exit arrow has been added to the chat selection menu.

--- src/screens/main/channel/InfoPanel.tsx
+++ src/screens/main/channel/InfoPanel.tsx
@@ -1,4 +1,5 @@
 import { type Dispatch, type SetStateAction } from "react";
+import { useWindowWidth } from "@hooks/useWindowWidth";
 import { type ChannelModel, type UserModel } from "../../Main";
 import classes from "./InfoPanel.module.css";
 
@@ -13,12 +14,23 @@ const InfoPanel = ({
     setSelectedChannel,
     users
 }: InfoPanelProps) => {
+    const isMobile = useWindowWidth() < 768;
     const channelName = users.find(user => user.id === channel.interlocutorId)?.fullName || "(^~^)";
     
     return (
         <div className={classes.container}>
-            <div className={classes.name}>{channelName}</div>
-            <div className={classes.status}>last seen recently</div>    
+            {isMobile && (
+                <button className={classes.buttonBack} onClick={() => setSelectedChannel(null)}>
+                    <svg className={classes.buttonBackIcon} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M23,11H3.41L12,2.41c.39-.39.39-1.02,0-1.41h0c-.39-.39-1.02-.39-1.41,0L.29,11.29h0c-.18.18-.29.43-.29.71s.11.53.29.71h0s10.29,10.29,10.29,10.29c.39.39,1.02.39,1.41,0h0c.39-.39.39-1.02,0-1.41L3.41,13h19.59c.55,0,1-.45,1-1h0c0-.55-.45-1-1-1Z"/>
+                    </svg>
+                </button>
+            )}
+            
+            <div className={classes.info}>
+                <div className={classes.name}>{channelName}</div>
+                <div className={classes.status}>last seen recently</div>    
+            </div>
         </div>
     )
 }
