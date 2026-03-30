ce24b772182ee5d87bbdbb89ecfad6757420f975
laconite <laconite2006@gmail.com>
2026-03-30 14:08:08 +0400

    Configured by PWA.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate', 
+      manifest: { 
+        name: 'Bitogram Web App', 
+        short_name: 'Bitogram Web', 
+        start_url: '/', 
+        display: 'standalone',
+        theme_color: '#101417',
+        background_color: '#101417',
+        icons: [{ 
+          src: '/logo-icon-192.png', 
+          sizes: '192x192', 
+          type: 'image/png' 
+        }, { 
+          src: '/logo-icon-512.png', 
+          sizes: '512x512', 
+          type: 'image/png' 
+        }] 
+      } 
+    })
+  ],
   resolve: {
     alias: {
       "@hooks": path.resolve(__dirname, "./src/hooks"),
